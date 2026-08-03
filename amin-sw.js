7c066761f4751dfc2e669cc9c80724f53d1c23ec
reasraa0-star <reasraa0@gmail.com>
2026-08-03 16:19:59 +0300

    Add files via upload

---
+++ amin-sw.js
@@ -0,0 +1,4 @@
+var C='amin-v1';
+self.addEventListener('install',function(e){e.waitUntil(caches.open(C).then(function(c){return c.addAll(['./amin.html','./amin-manifest.json'])}).then(function(){return self.skipWaiting()}))});
+self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.filter(function(x){return x!==C}).map(function(x){return caches.delete(x)}))}).then(function(){return self.clients.claim()}))});
+self.addEventListener('fetch',function(e){if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(function(h){return h||fetch(e.request).then(function(r){var cp=r.clone();caches.open(C).then(function(c){c.put(e.request,cp)});return r}).catch(function(){return caches.match('./amin.html')})}))});
